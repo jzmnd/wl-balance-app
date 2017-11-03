09ec67802e637591cc2149f8e45ddfb96caf854d
Jeremy Smith <j.smith.03@cantab.net>
2017-11-02 17:00:58 -0700

    Update

--- app/config.py
+++ app/config.py
@@ -7,10 +7,6 @@ DEBUG = True
 # Define the application directory
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-# Define the database
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-# DATABASE_CONNECT_OPTIONS = {}
-
 # Application threads
 THREADS_PER_PAGE = 2
 
